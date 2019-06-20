89f179762a60d20f6f27920e3946b4fd1998e29a
代利文 <450326779@qq.com>
2019-06-20 10:33:51 +0800

    框架搭建基本完成

---
+++ src/main/java/com/zr/demo/task/MyScheduler.java
@@ -0,0 +1,18 @@
+package com.zr.demo.task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+
+public class MyScheduler {
+	
+	 @Scheduled(cron = "* * * * * ?") // 每秒调用一次
+	    public void runJobB() {
+	        System.err.println("【*** MyTaskB - 间隔调度 ***】"
+	                + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+	                        .format(new Date()));
+	    }
+
+}
